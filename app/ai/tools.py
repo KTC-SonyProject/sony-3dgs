0c4f6c223505fca60b5ff7cc0752dde3935bd689
snow7y <ktc2243020002@edu.kyoto-tech.ac.jp>
2024-11-30 14:38:46 +0000

    displayのコマンドツールを作成

--- app/ai/tools.py
+++ app/ai/tools.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from langchain_core.documents import Document
 from langchain_core.tools import tool
 from pydantic import BaseModel, Field
@@ -11,7 +13,7 @@ class SearchDocumentInput(BaseModel):
     query: str = Field(description="ドキュメントを検索するクエリ")
 
 @tool("search_documents_tool", args_schema=SearchDocumentInput)
-def search_documents(query: str) -> list[Document]:
+def search_documents_tool(query: str) -> list[Document]:
     """
     ドキュメントを検索する関数
     この関数で取得したドキュメントをユーザーに返す場合は"[参考にしたドキュメント](metadataのsourceに格納されている数値)"のような形で返す
@@ -25,9 +27,47 @@ def search_documents(query: str) -> list[Document]:
     return results
 
 
-tools = [search_documents]
+class OperationStr(Enum):
+    next_scene = "次のシーン"
+    previous_scene = "前のシーン"
+    rotate_scene = "シーンを回転"
+
+
+class DisplayOperationInput(BaseModel):
+    operation: str = Field(
+        description=(
+            f"操作内容 操作は以下のいずれかの文字列で指定する: {', '.join([op.value for op in OperationStr])}"
+        )
+    )
+
+@tool("display_operation_tool", args_schema=DisplayOperationInput)
+def display_operation_tool(operation: str) -> str:
+    """
+    Displayの操作を行う関数
+    """
+    # TODO: ここにDisplayの操作を行うコードを記述する
+
+    if operation == OperationStr.next_scene.value:
+        # 次のシーンを表示する
+        pass
+    elif operation == OperationStr.previous_scene.value:
+        # 前のシーンを表示する
+        pass
+    elif operation == OperationStr.rotate_scene.value:
+        # シーンを回転する
+        pass
+    else:
+        raise ValueError(f"Invalid operation: {operation}")
+
+    return f"次の操作を行いました: {operation}"
+
+
+tools = [search_documents_tool, display_operation_tool]
 
 if __name__ == "__main__":
-    print(f"{search_documents.name=}, {search_documents.description=}, {search_documents.args=}")
+    print(f"{search_documents_tool.name=}, {search_documents_tool.description=}, {search_documents_tool.args=}")
+    print(f"{display_operation_tool.name=}, {display_operation_tool.description=}, {display_operation_tool.args=}")
 
-    print(search_documents.invoke("test"))
+
+    # print(search_documents_tool.invoke("test"))
+    print(display_operation_tool.invoke("次のシーン"))
